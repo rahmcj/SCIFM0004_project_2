563c74608644e012d4c1b976c04f33112e7fe6d5
rm13819@bristol.ac.uk <rm13819@bristol.ac.uk>
2024-04-21 16:51:27 +0100

    Added Dockerfiles for each container so that images can be made.

--- app/outputter/output.py
+++ app/outputter/output.py
@@ -1,32 +1,78 @@
 ### Might need to have it so all the data is added together here
 ### but also might be on main.py
 import pika
+import uproot
+import awkward as ak
+import vector
+import time
+import json
+import infofile
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import AutoMinorLocator  # for minor ticks
+
+# Define the merged data structure
+merged_data = {}
 
 ### Units ###
 MeV = 0.001
 GeV = 1.0
 
-### Plot data
+# Connect to RabbitMQ
+def rabbitmq_connect(host, retries=10, delay=5):
+    for i in range(retries):
+        try:
+            return pika.BlockingConnection(pika.ConnectionParameters(host=host))
+        except pika.exceptions.AMQPConnectionError:
+            print(f'Failed to connect to {host}, retrying in {delay} seconds')
+            time.sleep(delay)
+    raise Exception(f'Failed to connect to {host} after {retries} retries')
 
-def plot_data(data):
+# Function to process each segment of data
+def process_segment(segment):
+    sample = segment['sample']
+    data = segment['data']
 
+    if sample not in merged_data:
+        merged_data[sample] = data
+    else:
+        merged_data[sample] = ak.concatenate([merged_data[sample], data])
+
+# Callback function for receiving messages
+def callback(ch, method, properties, body):
+    segment = json.loads(body)
+    process_segment(segment)
+    print(f'Processed {segment["sample"]} segment')
+
+# Connect to RabbitMQ
+connection = rabbitmq_connect('localhost')
+channel = connection.channel()  
+channel.queue_declare(queue='segmented_data')   
+channel.basic_consume(queue='segmented_data', on_message_callback=callback, auto_ack=True)
+
+# Wait for messages
+print('Waiting for messages. To exit press CTRL+C')
+channel.start_consuming()
+
+# Plot the merged data
+def plot_data(merged_data):
     xmin = 80 * GeV
     xmax = 250 * GeV
     step_size = 5 * GeV
 
     bin_edges = np.arange(start=xmin, # The interval includes this value
-                     stop=xmax+step_size, # The interval doesn't include this value
-                     step=step_size ) # Spacing between values
+                                            stop=xmax+step_size, # The interval doesn't include this value
+                                            step=step_size ) # Spacing between values
     bin_centres = np.arange(start=xmin+step_size/2, # The interval includes this value
-                            stop=xmax+step_size/2, # The interval doesn't include this value
-                            step=step_size ) # Spacing between values
+                                            stop=xmax+step_size/2, # The interval doesn't include this value
+                                            step=step_size ) # Spacing between values
 
-    data_x,_ = np.histogram(ak.to_numpy(data['data']['mllll']), 
-                            bins=bin_edges ) # histogram the data
+    data_x,_ = np.histogram(ak.to_numpy(merged_data['data']['mllll']), 
+                                                 bins=bin_edges ) # histogram the data
     data_x_errors = np.sqrt( data_x ) # statistical error on the data
 
-    signal_x = ak.to_numpy(data[r'Signal ($m_H$ = 125 GeV)']['mllll']) # histogram the signal
-    signal_weights = ak.to_numpy(data[r'Signal ($m_H$ = 125 GeV)'].totalWeight) # get the weights of the signal events
+    signal_x = ak.to_numpy(merged_data[r'Signal ($m_H$ = 125 GeV)']['mllll']) # histogram the signal
+    signal_weights = ak.to_numpy(merged_data[r'Signal ($m_H$ = 125 GeV)'].totalWeight) # get the weights of the signal events
     signal_color = samples[r'Signal ($m_H$ = 125 GeV)']['color'] # get the colour for the signal bar
 
     mc_x = [] # define list to hold the Monte Carlo histogram entries
@@ -36,8 +82,8 @@ def plot_data(data):
 
     for s in samples: # loop over samples
         if s not in ['data', r'Signal ($m_H$ = 125 GeV)']: # if not data nor signal
-            mc_x.append( ak.to_numpy(data[s]['mllll']) ) # append to the list of Monte Carlo histogram entries
-            mc_weights.append( ak.to_numpy(data[s].totalWeight) ) # append to the list of Monte Carlo weights
+            mc_x.append( ak.to_numpy(merged_data[s]['mllll']) ) # append to the list of Monte Carlo histogram entries
+            mc_weights.append( ak.to_numpy(merged_data[s].totalWeight) ) # append to the list of Monte Carlo weights
             mc_colors.append( samples[s]['color'] ) # append to the list of Monte Carlo bar colors
             mc_labels.append( s ) # append to the list of Monte Carlo legend labels
     
@@ -50,8 +96,8 @@ def plot_data(data):
     
     # plot the data points
     main_axes.errorbar(x=bin_centres, y=data_x, yerr=data_x_errors,
-                       fmt='ko', # 'k' means black and 'o' is for circles 
-                       label='Data') 
+                                        fmt='ko', # 'k' means black and 'o' is for circles 
+                                        label='Data') 
     
     # plot the Monte Carlo bars
     mc_heights = main_axes.hist(mc_x, bins=bin_edges, 
@@ -65,15 +111,15 @@ def plot_data(data):
     
     # plot the signal bar
     main_axes.hist(signal_x, bins=bin_edges, bottom=mc_x_tot, 
-                   weights=signal_weights, color=signal_color,
-                   label=r'Signal ($m_H$ = 125 GeV)')
+                                 weights=signal_weights, color=signal_color,
+                                 label=r'Signal ($m_H$ = 125 GeV)')
     
     # plot the statistical uncertainty
     main_axes.bar(bin_centres, # x
-                  2*mc_x_err, # heights
-                  alpha=0.5, # half transparency
-                  bottom=mc_x_tot-mc_x_err, color='none', 
-                  hatch="////", width=step_size, label='Stat. Unc.' )
+                                2*mc_x_err, # heights
+                                alpha=0.5, # half transparency
+                                bottom=mc_x_tot-mc_x_err, color='none', 
+                                 hatch="////", width=step_size, label='Stat. Unc.' )
 
     # set the x-limit of the main axes
     main_axes.set_xlim( left=xmin, right=xmax ) 
@@ -83,17 +129,17 @@ def plot_data(data):
     
     # set the axis tick parameters for the main axes
     main_axes.tick_params(which='both', # ticks on both x and y axes
-                          direction='in', # Put ticks inside and outside the axes
-                          top=True, # draw ticks on the top axis
-                          right=True ) # draw ticks on right axis
+                                                 direction='in', # Put ticks inside and outside the axes
+                                                 top=True, # draw ticks on the top axis
+                                                  right=True ) # draw ticks on right axis
     
     # x-axis label
     main_axes.set_xlabel(r'4-lepton invariant mass $\mathrm{m_{4l}}$ [GeV]',
-                        fontsize=13, x=1, horizontalalignment='right' )
+                                             fontsize=13, x=1, horizontalalignment='right' )
     
     # write y-axis label for main axes
     main_axes.set_ylabel('Events / '+str(step_size)+' GeV',
-                         y=1, horizontalalignment='right') 
+                                             y=1, horizontalalignment='right') 
     
     # set y-axis limits for main axes
     main_axes.set_ylim( bottom=0, top=np.amax(data_x)*1.6 )
@@ -103,10 +149,10 @@ def plot_data(data):
 
     # Add text 'ATLAS Open Data' on plot
     plt.text(0.05, # x
-             0.93, # y
-             'ATLAS Open Data', # text
-             transform=main_axes.transAxes, # coordinate system used is that of main_axes
-             fontsize=13 ) 
+                0.93, # y
+                'ATLAS Open Data', # text
+                transform=main_axes.transAxes, # coordinate system used is that of main_axes
+                fontsize=13 ) 
     
     # Add text 'for education' on plot
     plt.text(0.05, # x
@@ -132,7 +178,9 @@ def plot_data(data):
     # draw the legend
     main_axes.legend( frameon=False ) # no box around the legend
     
-    return
+    pass
 
-### Call function to plot data
-plot_data(data)
+# Call the function to plot the data
+plot_data(merged_data)
+plt.show()
+
---
+++ app/producer/producer.py
@@ -0,0 +1,122 @@
+import uproot # for reading .root files
+import awkward as ak # to represent nested data in columnar format
+import time # to measure time to analyse
+import pika # for rabbitMQ
+import infofile # local file containing cross-sections, sums of weights, dataset IDs
+import json
+
+lumi = 10 # fb-1 # data_A,data_B,data_C,data_D
+fraction = 1.0 # reduce this is if you want the code to run quicker                                                                                                                         
+#tuple_path = "Input/4lep/" # local 
+tuple_path = "https://atlas-opendata.web.cern.ch/atlas-opendata/samples/2020/4lep/" # web address
+# Define number of workers to be used
+number_workers = 4
+
+
+    
+
+
+samples = {
+
+    'data': {
+        'list' : ['data_A','data_B','data_C','data_D'],
+    },
+
+    r'Background $Z,t\bar{t}$' : { # Z + ttbar
+        'list' : ['Zee','Zmumu','ttbar_lep'],
+        'color' : "#6b59d3" # purple
+    },
+
+    r'Background $ZZ^*$' : { # ZZ
+        'list' : ['llll'],
+        'color' : "#ff0000" # red
+    },
+
+    r'Signal ($m_H$ = 125 GeV)' : { # H -> ZZ -> llll
+        'list' : ['ggH125_ZZ4lep','VBFH125_ZZ4lep','WH125_ZZ4lep','ZH125_ZZ4lep'],
+        'color' : "#00cdff" # light blue
+    },
+
+}
+
+
+### Units ###
+MeV = 0.001
+GeV = 1.0
+
+# Define rabbitMQ connection
+def rabbitmq_connection(host, retries=10, delay=5):
+    for i in range(retries):
+        try:
+            return pika.BlockingConnection(pika.ConnectionParameters(host=host))
+        except pika.exceptions.AMQPConnectionError:
+            print(f"Failed to connect to {host}, retrying in {delay} seconds")
+            time.sleep(delay)
+    raise Exception(f"Failed to connect to {host} after {retries} retries")
+
+##Get data
+
+def get_data_from_files():
+
+    data = {} # define empty dictionary to hold awkward arrays
+    for s in samples: # loop over samples
+        frames = [] # define empty list to hold data
+        for val in samples[s]['list']: # loop over each file
+            if s == 'data': 
+                prefix = "Data/" # Data prefix
+            else: # MC prefix
+                prefix = "MC/mc_"+str(infofile.infos[val]["DSID"])+"."
+            fileString = tuple_path+prefix+val+".4lep.root" # file name to open
+            temp = read_file(fileString,val) # call the function read_file defined below
+            frames.append(temp) # append array returned from read_file to list of awkward arrays
+        data[s] = ak.concatenate(frames) # dictionary entry is concatenated awkward arrays
+    return data # return dictionary of awkward arrays
+
+## Segmenting data
+def segment_data(data, number_workers):
+    count_events = len(data)
+    segment_size = count_events // number_workers
+    start_and_end = [ ]
+    for i in range(number_workers):
+        start = i * segment_size
+        end = (i + 1) * segment_size + (count_events % number_workers > i)
+        start_and_end.append((start, end))
+    return start_and_end
+
+
+
+
+## Read file
+
+def read_file(path):
+    with uproot.open(path + ":mini") as tree:
+       data_all = tree.arrays() # read all data
+    return data_all
+
+
+def send_data_to_workers(start_and_end):
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
+    channel.queue_declare(queue='segmented_data')
+    for segment in start_and_end:
+        channel.basic_publish(exchange='', 
+                                                routing_key='segmented_data',
+                                                body=json.dumps(segment))
+    connection.close()
+    print("Data sent to consumers")
+    return
+
+### Need to get the data before defining start and end ####
+data = get_data_from_files()
+
+## Get start and end points for each worker
+start_and_end = segment_data(data, number_workers)  
+
+### Data processing
+send_data_to_workers(start_and_end)
+
+#start = time.time() # time at start of whole processing
+#data = get_data_from_files() # process all files
+#elapsed = time.time() - start # time after whole processing
+#print("Time taken: "+str(round(elapsed,1))+"s") # print total time taken to process every file
+
