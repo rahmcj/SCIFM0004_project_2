649a752f3979f86bc9a1370542d0a6b8a6add9fe
rm13819@bristol.ac.uk <rm13819@bristol.ac.uk>
2024-04-23 20:23:01 +0100

    Added a timer in to the consumer file to time the processes.

--- app/consumer/consumer.py
+++ app/consumer/consumer.py
@@ -110,6 +110,8 @@ def process_segment(field_list, tuple_path):
     fileString = tuple_path+pref+val+".4lep.root" # file name to open
     
     data_all = [] # empty list to hold data
+
+    start = time.time() # start timer
     # open the tree called mini using a context manager (will automatically close files/resources)
     with uproot.open(fileString + ":mini") as tree:
         numevents = tree.num_entries # number of events
@@ -146,14 +148,14 @@ def process_segment(field_list, tuple_path):
 
         
             data_all.append(data) # append array from this batch
-     
+            total_time = time.time() - start # calculate total time taken
     
     data = ak.concatenate(data_all) # concatenate all of the arrays
     ser_data = ak.to_list(data) # serialize the awkward array
     data_and_sample = { 'sample': sample, 'data': ser_data } # create dictionary with sample name and data
     pre_compressed_data = json.dumps(data_and_sample) # convert dictionary to JSON
     compressed_data = zlib.compress(pre_compressed_data.encode('utf-8')) # compress serialised data
-
+    print(total_time) # print total time taken for comparison
     return compressed_data
 
 def callback(ch, method, properties, body):
